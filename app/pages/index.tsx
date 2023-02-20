699175ae8d47728d19cd817dab357f9bc4ff97ec
ulikabbq <ulikabbq@gmail.com>
2023-02-20 20:11:25 +0000

    add header css

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -10,24 +10,18 @@ const getImageUrl = (): string => {
   return imageUrl;
 };
 
-const Header: React.FC = () => {
-  return (
-    <header className="header">
-      <Link href="https://www.youtube.com/channel/UCSy-iP7qqR7Grx9r4qgIK7g">
-        <a className="logo-link">
-          <img src="http://rufusmovies.s3.amazonaws.com/youtube-logo.png" alt="YouTube logo" className="logo" />
-        </a>
-      </Link>
-    </header>
-  );
-};
-
 const HomePage: React.FC = () => {
   const imageUrl = getImageUrl();
   return (
     <div>
-      <Header />
-      <Image src={imageUrl} alt="Movie of the Day" layout="fill" objectFit="contain" />
+      <header>
+        <Link href="https://www.youtube.com/">
+          <a>
+            <img src="http://rufusmovies.s3.amazonaws.com/youtube-logo.png" alt="YouTube" />
+          </a>
+        </Link>
+      </header>
+      <Image src={imageUrl} alt="Rufus Movie of the Day" />
     </div>
   );
 };
